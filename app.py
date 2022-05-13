536df39513452147432956dc6a7c9e5fd3b00f7f
Havoi <95433996+Havoi@users.noreply.github.com>
2022-05-13 17:59:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify
+
+import json
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return "this is a simple imgur api with web scraping . \n You can use this by passing gallery id in the url of this website \n. for example : https://imgur-api-scraper.herokuapp.com/{gallery_id}"
+@app.route("/<gallery_id>")
+def hello_world(gallery_id):
+    
+    url = f'https://imgur.com/gallery/{gallery_id}'
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content,'html.parser')
+
+    data = soup.find('script')
+    data = data.text
+    data = data.replace('<script>', '')
+    data = data.replace("\\" , '')
+    data = data.replace('window.postDataJSON=' , '')
+    data = data[1:-1]
+    data = data.replace('\n' , '')
+
+    data = json.loads(data)
+    return jsonify(data)
+    
+
+
+if __name__ == '__main__':
+   app.run(host="192.168.1.6",port=8800, debug=  True)
+
+
